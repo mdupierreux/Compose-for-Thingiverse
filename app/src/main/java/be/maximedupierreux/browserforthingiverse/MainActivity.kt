0b7ea0adfb2f77b9f7c28edd5cfb96a8191845f9
mdupierreux <maxime.dupierreux@gmail.com>
2022-05-09 08:18:45 +0200

    added appbar & FAB

--- app/src/main/java/be/maximedupierreux/browserforthingiverse/MainActivity.kt
+++ app/src/main/java/be/maximedupierreux/browserforthingiverse/MainActivity.kt
@@ -16,9 +16,16 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -26,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import be.maximedupierreux.browserforthingiverse.network.model.Creator
@@ -53,22 +61,34 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun ThingsList(viewModel: ThingsViewModel) {
         val things by viewModel.things.collectAsState()
-
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(4.dp),
+        Scaffold(
+            topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) }) },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = {},
+                    backgroundColor = MaterialTheme.colors.secondary,
+                    contentColor = MaterialTheme.colors.onSecondary
+                ) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = null)
+                }
+            }
         ) {
-            itemsIndexed(things) { index, thing ->
-                ThingRow(
-                    thing = thing,
-                    modifier = Modifier.padding(horizontal = 8.dp)
-                )
-                if (index < things.lastIndex) {
-                    Divider(
-                        color = MaterialTheme.colors.onBackground.copy(alpha = .3f),
-                        thickness = 0.5.dp,
-                        modifier = Modifier.padding(vertical = 8.dp)
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+                itemsIndexed(things) { index, thing ->
+                    ThingRow(
+                        thing = thing,
+                        modifier = Modifier.padding(horizontal = 8.dp)
                     )
+                    if (index < things.lastIndex) {
+                        Divider(
+                            color = MaterialTheme.colors.onBackground.copy(alpha = .3f),
+                            thickness = 0.5.dp,
+                            modifier = Modifier.padding(vertical = 8.dp)
+                        )
+                    }
                 }
             }
         }
